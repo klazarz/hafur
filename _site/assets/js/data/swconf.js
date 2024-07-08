ad642d1b57f380d527946649598986c0931208c0
Kevin Lazarz <kevin.lazarz@oracle.com>
2024-07-08 06:40:25 +0200

    Revert "2024-07-07"
    This reverts commit b40a99e26d0f962a429b7a03230c517311561da9.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719602254',resources: [
+    cacheName: 'chirpy-1719421717',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
